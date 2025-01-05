6a0e6fee8d57618b611304844ab6055fdac1a560
ptiforka <35385354+ptiforka@users.noreply.github.com>
2025-01-05 12:29:08 +0100

    Update ln_op.sh

--- ln_op.sh
+++ ln_op.sh
@@ -15,7 +15,12 @@ sudo apt install -y xvfb
 
 # Add Docker's official GPG key and repository
 echo "Adding Docker GPG key and repository..."
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+if [[ -f "/usr/share/keyrings/docker-archive-keyring.gpg" ]]; then
+    echo "Docker GPG keyring already exists. Skipping overwrite."
+else
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+fi
+
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install Docker
