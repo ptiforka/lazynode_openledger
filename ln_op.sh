6a646713f7bfba6194845c7f5e9f052b333e0ef6
ptiforka <35385354+ptiforka@users.noreply.github.com>
2025-01-05 12:23:33 +0100

    Update ln_op.sh

--- ln_op.sh
+++ ln_op.sh
@@ -12,17 +12,18 @@ echo "Installing required dependencies..."
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 sudo apt install -y xvfb
 
-# Install Docker
-echo "Installing Docker..."
-sudo apt -y update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
-sudo docker --version
 
 # Add Docker's official GPG key and repository
 echo "Adding Docker GPG key and repository..."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+# Install Docker
+echo "Installing Docker..."
+
+sudo apt -y update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo docker --version
 
 
 # Install additional dependencies
